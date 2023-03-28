98b6156a074fcafde481b361b70315ca0a13a9b0
Daphne <daphnesofiaglez@gmail.com>
2023-03-27 20:02:07 -0600

    Ejercicios_JS

---
+++ Ejercicio2/funciones.js
@@ -0,0 +1,15 @@
+function hello(){
+    console.log('Hello!');
+    console.log('Welcome to Js');
+}
+
+function imprime(msj, n){
+    for(var i=0; i<n; i++){
+        console.log( i + ' '+ msj);
+    }
+}
+
+hello();
+
+imprime('tqm', 10);
+
